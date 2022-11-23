219aa4b0c10cbfe070470041260964e67ddbc30a
kht9938 <114565370+kht9938@users.noreply.github.com>
2022-11-23 15:52:11 +0900

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,31 +1,118 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.text.ParseException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+// 네이버 기계번역 (Papago SMT) API 예제
 public class Test {
-	public static void main(String[] args) throws ParseException {
-		
-		String URL = "https://www.google.com/search?q=시퍼런봄+lyrics";
-		Document doc;
 
-		try {
-			doc = Jsoup.connect(URL).get();
-			Elements elem = doc.select(".date");
-			String[] str = elem.text().split(" ");
+    public static void main(String[] args) throws ParseException {
+        String clientId = "6JZ7GA8pXEXwOIO98N7W";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "CifxfDE4hU";//애플리케이션 클라이언트 시크릿값";
 
-			Elements lyrics =doc.select(".xaAUmb");
-			
-			String ly = lyrics.get(0).text();
-			
-			System.out.println(ly);
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        try {
+            text = URLEncoder.encode("Pure emotion\r\n"
+            		+ "Let it burn\r\n"
+            		+ "Like fire\r\n"
+            		+ "Do you yearn for a change?\r\n"
+            		+ "And I hope that you learn\r\n"
+            		+ "To never make the same mistake", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        String responseBody = post(apiURL, requestHeaders, text);
+
+        //System.out.println(responseBody);
+        
+        JSONParser jsonParser = new JSONParser();
+
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(responseBody);
+        //System.out.println(jsonObject);
+        
+        JSONObject objMessage = (JSONObject) jsonObject.get("message");
+        //System.out.println(objMessage);
+        
+        JSONObject objResult= (JSONObject) objMessage.get("result");
+        //System.out.println(objResult);
+        
+        String translatedText = (String) objResult.get("translatedText");
+        System.out.println(translatedText);
+        
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=en&target=ko&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
 }
